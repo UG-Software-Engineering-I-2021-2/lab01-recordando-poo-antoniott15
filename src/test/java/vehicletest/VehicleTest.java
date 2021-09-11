6e7736a5ac89558acb6565116ce5b9f8ebcb6ae3
antoniott15 <antonio.toche@gmail.com>
2021-09-10 19:54:43 -0500

    feat(test): add test in parallel, count 80)

--- src/test/java/vehicletest/VehicleTest.java
+++ src/test/java/vehicletest/VehicleTest.java
@@ -32,7 +32,7 @@ public class VehicleTest {
     public void TestTruckWhileRefuelFail() {
         Vehicle truck;
         truck = new Truck(20,100);
-        Assert.assertEquals(truck.supplying(200),"Camíon  no  se  puede  reabastecer  el  tanque,  sobrepasa la capacidad");
+        Assert.assertEquals(truck.supplying(200),"Camíon no se puede reabastecer el tanque, sobrepasa la capacidad");
     }
 
 
